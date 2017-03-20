64e00a674b5bcd4ab0d4f034500968eb3043670b
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-03-20 00:45:36 -0400

    Added a test folder, the packages in it, and a TaskTest JUnitTest Case

---
+++ Project2/test/edu/ncsu/csc216/backlog/task/TaskTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.backlog.task;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
